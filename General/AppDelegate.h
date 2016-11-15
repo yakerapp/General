5e0cc6d938e2228eeba220fa0cab41eef7e83aea
Yaker <yakerwei@126.com>
2016-11-15 11:45:01 +0800

    Initial Commit

---
+++ General/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  General
+//
+//  Created by yaker on 2016/11/15.
+//  Copyright © 2016年 yaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
